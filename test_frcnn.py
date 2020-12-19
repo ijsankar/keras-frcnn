b8568738a859580145784e492bd30c9f3b87d3cc
Sankar <ijsankar@outlook.com>
2020-12-19 22:17:07 +0530

    Update test_frcnn.py

--- test_frcnn.py
+++ test_frcnn.py
@@ -244,5 +244,5 @@ for idx, img_name in enumerate(sorted(os.listdir(img_path))):
 	print('Elapsed time = {}'.format(time.time() - st))
 	print(all_dets)
 	cv2_imshow(img)
-	cv2.waitKey(0)
 	cv2.imwrite('./results_imgs/{}.png'.format(idx),img)
+	cv2.waitKey(0)
